fed936655f173e4d7faa7c38bd2860c442ae98e2
DeniseOdeth18 <89421213+DeniseOdeth18@users.noreply.github.com>
2022-01-12 19:11:29 -0600

    Add files via upload

---
+++ js/Cannon.js
@@ -0,0 +1,17 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    
+  }
+  display() {
+
+    //codigo para crear la parte superior del cañon
+   
+    //codigo para crear la parte inferior del cañon
+  }
+
+}
